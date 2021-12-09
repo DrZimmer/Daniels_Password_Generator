e50a8791edd7c73a3e68fef6f5bcdc1fdcb2d831
White Tiger <dayley.zimmermann@gmail.com>
2021-12-08 21:02:53 -0500

    notes from class

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,18 @@
 // Assignment code here
 
+var special_chars = [""]
+var lowercase_letters = [""]
+var uppercase_letters = [""]
+var numeric = [1, 2, 3, 4]
+var possible_characters = [all vars above separated by comma]
+var test = lowercaseletters concat
 
-
+var password = ''
+for (var i = 0; i < 3; i++) {
+  var randomChar = possible_characters[3];
+  var randomChar = possible_characters[math.floor(Math.random() * possible_characters.length + 1)];
+  password = password + String(randomChar)
+};
 
 
 
