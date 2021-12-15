9b04c75230cda1676aa5f6181c29a3e9d173e68b
White Tiger <dayley.zimmermann@gmail.com>
2021-12-15 14:36:35 -0500

    added loads of pseudocode for how to do this

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,26 @@
 // Assignment code here
 
 var specialCharacters =  ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'];
-var lowercaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
-var uppercaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z";
+var lowercaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var uppercaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numeric = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var possible_characters = [lowercaseLetters, uppercaseLetters, numeric, specialCharacters]
+//So, you create an object that should store these properties
+// PasswordLength, - Number
+// HasUpperCase, - Boolean
+// HasLowerCase, - Boolean
+// HasNumber, - Boolean
+// HasSymbol - Boolean 
+// //Then you in your password generator function,  
+// //you check if any of these Boolean properties is true,
+//you add the letters, or symbols, or numbers or lowercase in to an array
+var passwordRequirements = function() {
+  
+  
+  writePassword();
+  generatePassword();
+}
+
 //var test = lowercaseletters concat
 
 var password = ''
@@ -13,12 +29,6 @@ for (var i = 0; i < 3; i++) {
   var randomChar = possible_characters[math.floor(Math.random() * possible_characters.length + 1)];
   password = password + String(randomChar)
 };
-
-var passwordRequirements = function() {
-
-
-  generatePassword();
-}
 
 var generatePassword = function () {
 
@@ -39,6 +49,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-writePassword();
+generateBtn.addEventListener("click", passwordRequirements);
