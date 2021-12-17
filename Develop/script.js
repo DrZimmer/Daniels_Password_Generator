b57dfbff66ff7bb0612f5e46f9e2e09bf02dca3b
White Tiger <dayley.zimmermann@gmail.com>
2021-12-16 21:18:25 -0500

    added all prompts to beginning of generate password function with conditional statements

--- Develop/script.js
+++ Develop/script.js
@@ -12,18 +12,44 @@ var characterArr = [specialCharacters, lowercaseLetters, uppercaseLetters, numer
 //you add the letters, or symbols, or numbers or lowercase in to an array!!!
 
 var generatePassword = function () {
-  //if hasUpperCase is true do this (randomize and pick)
-  //if hasLowerCase ios true, ^
-//if hasNumber is true, ^
-//if hasSymbol is true, ^
-const length = prompt('How many characters would you like your password to be?', '8-128');
-if (length >= 8 && length <= 128) {
-  window.alert("You have selected " + length + " characters for your password")
-} else {
-  window.alert("Please select a number between or including 8 and 128");
-  return(generatePassword())
-  };
+  //Ask user how many characters to include in the generated password
+  const length = prompt('How many characters would you like your password to be?', '8-128');
+  if (length >= 8 && length <= 128) {
+    window.alert("You have selected " + length + " characters for your password.")
+  } else {
+    window.alert("Please select a number between or including 8 and 128");
+    return(generatePassword())
+    };
+  //Ask user if they want special, number, lowercased, uppercased characters in the generated password
+  const hasSpecialCharacters = confirm('Would you like to include special characters in your password? Ok for YES, cancel for NO.');
+    if (hasSpecialCharacters === true) {
+      window.alert("You have chosen to include special characters in your password");
+    } else {
+      window.alert("You have chosen NOT to include special characters in your password");
+    }
+  const hasNumberCharacters = confirm('Would you like to include number characters in your password? Ok for YES, cancel for NO.');
+    if (hasNumberCharacters === true) {
+      window.alert("You have chosen to include number characters in your password");
+    } else {
+      window.alert("You have chosen NOT to include number characters in your password");
+    }
+  const hasLowerCasedCharacters = confirm('Would you like to include lower case characters in your password? Ok for YES, cancel for NO.');
+    if (hasLowerCasedCharacters === true) {
+      window.alert("You have chosen to include lower case characters in your password");
+    } else {
+      window.alert("You have chosen NOT to include lower case characters in your password");
+    }
+  const hasUpperCasedCharacters = confirm('Would you like to include upper case characters in your password? Ok for YES, cancel for NO.');
+    if (hasUpperCasedCharacters === true) {
+      window.alert("You have chosen to include upper case characters in your password");
+    } else {
+      window.alert("You have chosen NOT to include upper case characters in your password");
+    }
 
+console.log(hasNumberCharacters);
+console.log(hasLowerCasedCharacters);
+console.log(hasUpperCasedCharacters);
+console.log(hasSpecialCharacters);
 console.log(length);
 };
 
