fbfcf62598c8ce6974703cb138657845648c08c9
White Tiger <dayley.zimmermann@gmail.com>
2021-12-17 13:54:03 -0500

    working on generating password for loops

--- Develop/script.js
+++ Develop/script.js
@@ -4,17 +4,13 @@ var specialCharacters =  ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',','
 var lowercaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var uppercaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numeric = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']; 
-var characterArr = [specialCharacters, lowercaseLetters, uppercaseLetters, numeric];
 
-// //in your generatePassword function,  
-// //you check if any of these Boolean properties above are true,
-//you add the letters, or symbols, or numbers or lowercase in to an array!!!
-
-var generatePassword = function () {
+var passwordRequirements = function () {
+  
   //Ask user how many characters to include in the generated password
-  const length = prompt('How many characters would you like your password to be?', '8-128');
-  if (length >= 8 && length <= 128) {
-    window.alert("You have selected " + length + " characters for your password.")
+  const passwordLength = prompt('How many characters would you like your password to be?', '8-128');
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    window.alert("You have selected " + passwordLength + " characters for your password.")
   } else {
     window.alert("Please select a number between or including 8 and 128");
     return(generatePassword())
@@ -32,59 +28,83 @@ var generatePassword = function () {
     } else {
       window.alert("You have chosen NOT to include number characters in your password");
     }
-  const hasLowerCasedCharacters = confirm('Would you like to include lower case characters in your password? Ok for YES, cancel for NO.');
+    const hasLowerCasedCharacters = confirm('Would you like to include lower case characters in your password? Ok for YES, cancel for NO.');
     if (hasLowerCasedCharacters === true) {
       window.alert("You have chosen to include lower case characters in your password");
     } else {
       window.alert("You have chosen NOT to include lower case characters in your password");
     }
-  const hasUpperCasedCharacters = confirm('Would you like to include upper case characters in your password? Ok for YES, cancel for NO.');
+    const hasUpperCasedCharacters = confirm('Would you like to include upper case characters in your password? Ok for YES, cancel for NO.');
     if (hasUpperCasedCharacters === true) {
       window.alert("You have chosen to include upper case characters in your password");
     } else {
       window.alert("You have chosen NOT to include upper case characters in your password");
     };
-  if (hasSpecialCharacters === false && hasNumberCharacters === false && hasLowerCasedCharacters === false && hasUpperCasedCharacters === false) {
-    window.alert("You haven't chosen ANY character types! START OVER!");
-    return(generatePassword());
-  }
-  //next lines here
+    if (hasSpecialCharacters === false && hasNumberCharacters === false && hasLowerCasedCharacters === false && hasUpperCasedCharacters === false) {
+      window.alert("You haven't chosen ANY character types! START OVER!");
+      return(generatePassword());
+    }
+    //passwordRequirements object to be referenced in generate password
+    const passwordRequirementsObj = {
+      passwordLength: passwordLength,
+      hasSpecialCharacters: hasSpecialCharacters,
+      hasNumberCharacters: hasNumberCharacters,
+      hasLowerCasedCharacters: hasLowerCasedCharacters,
+      hasUpperCasedCharacters: hasUpperCasedCharacters
+    };
+    return passwordRequirementsObj;
+  };
 
-console.log(hasNumberCharacters);
-console.log(hasLowerCasedCharacters);
-console.log(hasUpperCasedCharacters);
-console.log(hasSpecialCharacters);
-console.log(length);
+// //in your generatePassword function,  
+// //you check if any of these Boolean properties above are true,
+//you add the letters, or symbols, or numbers or lowercase in to an array!!!
+
+var generatePassword = function () {
+
+  var requirements = passwordRequirements();
+
+  var finalPassword = [];
+
+  var maybeCharacters = [];
+
+  var mustHaveCharacters = [];
+
+  //check if password requirements object exists, if not exit.
+  if (!requirements) return null;
+
+  if (requirements.hasUpperCasedCharacters) {
+    maybeCharacters = maybeCharacters.concat(uppercaseLetters);
+    mustHaveCharacters.push(getRandom(uppercaseLetters));
+  }
+  if (requirements.hasUpperCasedCharacters) {
+    maybeCharacters = maybeCharacters.concat(uppercaseLetters);
+    mustHaveCharacters.push(getRandom(uppercaseLetters));
+  }
+  if (requirements.hasUpperCasedCharacters) {
+    maybeCharacters = maybeCharacters.concat(uppercaseLetters);
+    mustHaveCharacters.push(getRandom(uppercaseLetters));
+  }
+  if (requirements.hasUpperCasedCharacters) {
+    maybeCharacters = maybeCharacters.concat(uppercaseLetters);
+    mustHaveCharacters.push(getRandom(uppercaseLetters));
+  }
+
+  for(var i = 0; i < maybeCharacters.length; i++) {
+    //first for loop
+  };
+  for(var i = 0; i < mustHaveCharacters.length; i++) {
+    finalPassword[i] = mustHaveCharacters[i];
+  }
 };
 
-//OBJECT TO BE CREATED
-// var passwordRequirements = {
-//   length: length,
-//   hasSpecialCharacters: hasSpecialCharacters,
-//   hasNumberCharacters: hasNumberCharacters,
-//   hasLowerCasedCharacters: hasLowerCasedCharacters,
-//   hasUpperCasedCharacters: hasUpperCasedCharacters
-// };
 
 //WORK ON THIS RANDOM FUNCTION TO 
-function random(characterArr) {
-  let value = Math.floor(Math.random() * lowercaseLetters.length);
+function random(passwordRequirementsObj) {
+  let value = Math.floor(Math.random() * lowercaseLetters.passwordLength);
   value = lowercaseLetters[value]
   return value;
 };
-// function getRandomUpper() {
-  //   let value = Math.floor(Math.random() * lowercaseLetters.length);
-  //   value = uppercaseLetters[value]
-  //   return value;
-  // };
-  
-  //var test = lowercaseletters concat HAVE TO USE THIS SOMEWHERE?
-  
-  //and this
-  // var password = ''
-  // for (var i = 0; i < 3; i++) {
-    //   var randomChar = possible_characters[3];
-    //   var randomChar = possible_characters[math.floor(Math.random() * possible_characters.length + 1)];
+    //   var randomChar = possible_characters[math.floor(Math.random() * possible_characters.passwordLength + 1)];
     //   password = password + String(randomChar)
     // };
     
