e95a86c74d5a478d71c0b049299a6c3099e72753
White Tiger <dayley.zimmermann@gmail.com>
2021-12-16 20:34:30 -0500

    major overhaul to the organization of the code, including removals of comments, deleting a function so that generate password is the main function

--- Develop/script.js
+++ Develop/script.js
@@ -4,41 +4,40 @@ var specialCharacters =  ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',','
 var lowercaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var uppercaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numeric = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']; 
+var length = ''
 
-//So, you create an object that should store these properties
-// PasswordLength, - Number
-// HasUpperCase, - Boolean
-// HasLowerCase, - Boolean
-// HasNumber, - Boolean
-// HasSymbol - Boolean 
-// //Then you in your password generator function,  
+var passwordRequirements = {
+  length: length,
+  hasSpecialCharacters: hasSpecialCharacters,
+  hasNumberCharacters: hasNumberCharacters,
+  hasLowerCasedCharacters: hasLowerCasedCharacters,
+  hasUpperCasedCharacters: hasUpperCasedCharacters
+};
+// //in your password generator function,  
 // //you check if any of these Boolean properties is true,
-//you add the letters, or symbols, or numbers or lowercase in to an array
+//you add the letters, or symbols, or numbers or lowercase in to an array!!!
 
-// var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
-
-//   // Conditional Recursive Function Call
-//   if (promptFight === "" || promptFight === null) {
-//     window.alert("You need to provide a valid answer! Please try again.");
-//     return fightOrSkip();
-//   }
-
-//function to receive user input and create object that stores their preferences
-var passwordRequirements = function() {
-  var promptLength = window.prompt('How many characters would you like your password to be? (8-128)')
-  if (promptLength >= 8 && promptLength<= 128) {
-    const passwordLength = promptLength
+var generatePassword = function () {
+//set password length = to password length selected
+//if hasUpperCase is true do this (randomize and pick)
+//if hasLowerCase ios true, ^
+//if hasNumber is true, ^
+//if hasSymbol is true, ^
+var tenure = prompt("Please enter preferred tenure in years", "15");
+    
+if (tenure != null) {
+    alert("You have entered " + tenure + " years" );
+}
+  const length = window.prompt('How many characters would you like your password to be?', '8-128');
+  if (length >= 8 && length <= 128) {
+    const length = length;
   } else {
     window.alert("Please select a number between or including 8 and 128");
-    return passwordRequirements();
-  }
+    return generatePassword();
+  };
 
-  var passwordVariablesObj = [ {
-
-  }]
-  console.log(passwordLength)
-  writePassword();
-  generatePassword();
+  console.log(generatePassword());
+  console.log(length);
 };
 
 function getRandomLower() {
@@ -53,23 +52,9 @@ function getRandomUpper() {
   return value;
 };
 
-console.log(getRandomLower());
-console.log(getRandomUpper());
+//var test = lowercaseletters concat HAVE TO USE THIS SOMEWHERE?
 
-//turn this into an object so i can just do one function
-//passwordRequirements [{upper: [list of chars], lower: [list of chars], etc}]
-
-var passwordRequirements = {
-  length: length,
-  hasSpecialCharacters: hasSpecialCharacters,
-  hasNumberCharacters: hasNumberCharacters,
-  hasLowerCasedCharacters: hasLowerCasedCharacters,
-  hasUpperCasedCharacters: hasUpperCasedCharacters
-};
-
-
-//var test = lowercaseletters concat
-
+//and this
 // var password = ''
 // for (var i = 0; i < 3; i++) {
 //   var randomChar = possible_characters[3];
@@ -77,14 +62,6 @@ var passwordRequirements = {
 //   password = password + String(randomChar)
 // };
 
-var generatePassword = function () {
-//set password length = to password length selected
-//if hasUpperCase is true do this (randomize and pick)
-//if hasLowerCase ios true, ^
-//if hasNumber is true, ^
-//if hasSymbol is true, ^
-
-}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -99,4 +76,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", passwordRequirements());
+generateBtn.addEventListener("click", writePassword);
